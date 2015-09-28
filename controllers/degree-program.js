bf1b8f2fa83898c79a8457db68f1cebbf7c7e308
fuyuwinter1 <jgjaro@up.edu.ph>
2015-09-29 04:25:48 +0800

    'EXER4'

--- controllers/degree-program.js
+++ controllers/degree-program.js
@@ -22,9 +22,18 @@ exports.findOne = function (req, res, next) {
 
 // Insert a degree program.
 exports.insert = function (req, res, next) {
-	db.query("INSERT INTO degree(degree_code,description) VALUES (?,?)", [req.body.code,req.body.name], function (err, rows) {
+	
+	var code = req.body.code;
+	var name = req.body.name;
+	var unit = req.body.units;
+	
+	db.query("INSERT INTO degree(degree_code,description,total_units) VALUES (?,?,?)", [code,name,unit], function (err, rows) {
 		if(err) return(err);
 		res.send(rows);
+		return({"degree_code": code,
+				"description": name,
+				"total_units": unit});
+
 	});
 }
 
@@ -40,7 +49,7 @@ exports.remove = function (req, res, next) {
 
 // Update a degree program.
 exports.update = function (req, res, next) {
-	db.query("UPDATE degree SET total_units='"+req.body.units+"' WHERE degree_id='"+req.params.id+"'", function (err, rows) {
+	db.query("UPDATE degree SET total_units='"+req.body.units+"', WHERE degree_id='"+req.params.id+"'", function (err, rows) {
 		if(err) return(err);
 		if(rows.length === 0)
 			res.send(404, {message: 'Degree program not found.'});
