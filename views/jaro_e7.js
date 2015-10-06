71d8e0c55f146e79003d3093fdfc0cf2084b2e3a
fuyuwinter1 <jgjaro@up.edu.ph>
2015-10-06 14:43:47 +0800

    Exercise 7 EF-2L

---
+++ views/jaro_e7.js
@@ -0,0 +1,33 @@
+function car(PlateNo, Model, Color){
+	this.PlateNo = PlateNo;
+	this.Model = Model;
+	this.Color = Color;
+	this.Speed = 0;
+	
+	this.startCar = startCar;
+	this.stopCar = stopCar;
+	this.accelerate = accelerate;
+	this.decelerate = decelerate;
+	this.repaintCar = repaintCar;
+
+}
+
+function startCar(){
+	this.Speed = 10;
+}
+
+function stopCar(){
+	this.Speed = 0;
+}
+
+function accelerate(n){
+	this.Speed = this.Speed+n;
+}
+
+function decelerate(n){
+	this.Speed = this.Speed-n;
+}
+
+function repaintCar(newColor){
+	this.Color = newColor;
+}
